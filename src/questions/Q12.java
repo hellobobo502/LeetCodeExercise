785256d48b73c5097d3e3de0f38f7b8e1e855ec1
Yifu <hellobobo502@gmail.com>
2023-02-22 16:40:53 +0800

    final Q12

--- src/questions/Q12.java
+++ src/questions/Q12.java
@@ -6,15 +6,24 @@ package questions;
 public class Q12 {
 
 	public static void main(String[] args) {
-		int num = 1994;
+		int num = 2322;
 		System.out.println(intToRoman(num));
 	}
 
 	public static String intToRoman(int num) {
-		String[] m = {"", "M", "MM", "MMM"};
-		String[] c = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-		String[] x = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-		String[] i = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-		return m[(num%10000)/1000] + c[(num%1000)/100] + x[(num%100)/10] + i[num%10];
+		StringBuilder sb = new StringBuilder();
+		int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+		String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+		
+		for(int i=0; i<nums.length && num > 0; i++) {
+			if(num >= nums[i]) {
+				for(int j = num / nums[i]; 0 < j; j--) {
+					sb.append(romans[i]);
+					//num -= nums[i];
+				}
+				num = num % nums[i];
+			}
+		}
+		return sb.toString();
     }
 }
